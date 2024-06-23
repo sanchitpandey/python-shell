f06127c446507bd4c16e9b74c50236749ca99485
Sanchit Pandey <futurestone6@gmail.com>
2024-06-23 17:02:01 +0530

    run exec

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import subprocess
 
 def main():
     builtin = ["echo", "exit", "type"]
-    exec = []
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -11,26 +12,42 @@ def main():
         paths = os.getenv("PATH").split(":")
 
         x = input()
-        if (x == "exit 0"):
+        if x == "exit 0":
             exit(0)
-        elif (x == "exit 1"):
+        elif x == "exit 1":
             exit(1)
-        elif (x[:4]=="echo"):
-            sys.stdout.write(x[5:]+"\n")
-        elif (x[:4]=="type"):
-            if (x[5:] in builtin):
-                sys.stdout.write(x[5:]+" is a shell builtin\n")
+        elif x[:4] == "echo":
+            sys.stdout.write(x[5:] + "\n")
+        elif x[:4] == "type":
+            command = x[5:]
+            if command in builtin:
+                sys.stdout.write(command + " is a shell builtin\n")
             else:
-                found = 0;
+                found = False
                 for path in paths:
-                    if os.path.isfile(path+"/"+x[5:]):
-                        found = 1;
-                        sys.stdout.write(x[5:]+" is "+path+"/"+x[5:]+"\n")
-                if (found == 0):
-                    sys.stdout.write(x[5:]+": not found\n")
+                    executable_path = os.path.join(path, command)
+                    if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+                        found = True
+                        sys.stdout.write(command + " is " + executable_path + "\n")
+                        break
+                if not found:
+                    sys.stdout.write(command + ": not found\n")
         else:
-            sys.stdout.write(x+": command not found\n")
-
+            command_parts = x.split()
+            command = command_parts[0]
+            args = command_parts[1:]
+            found = False
+            for path in paths:
+                executable_path = os.path.join(path, command)
+                if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+                    found = True
+                    try:
+                        subprocess.run([executable_path] + args)
+                    except Exception as e:
+                        sys.stdout.write(f"Error executing {command}: {e}\n")
+                    break
+            if not found:
+                sys.stdout.write(command + ": command not found\n")
 
 if __name__ == "__main__":
     main()
