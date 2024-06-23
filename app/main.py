2c7134c03ff0b5ab787a375a210e7bd0a7ea27a8
Sanchit Pandey <futurestone6@gmail.com>
2024-06-23 16:53:25 +0530

    fixed path

--- app/main.py
+++ app/main.py
@@ -16,18 +16,20 @@ def main():
         elif (x == "exit 1"):
             exit(1)
         elif (x[:4]=="echo"):
-            sys.stdout.write(x[5:])
+            sys.stdout.write(x[5:]+"\n")
         elif (x[:4]=="type"):
             if (x[5:] in builtin):
-                sys.stdout.write(x[5:]+" is a shell builtin")
+                sys.stdout.write(x[5:]+" is a shell builtin\n")
             else:
+                found = 0;
                 for path in paths:
-                    for cmd in os.listdir(path):
-                        if (x[5:] == cmd):
-                            sys.stdout.write(x[5:]+" is "+path+cmd)
-                sys.stdout.write(x[5:]+"is ")
+                    if os.path.isfile(path+"/"+x[5:]):
+                        found = 1;
+                        sys.stdout.write(x[5:]+"is "+path+"/"+x[5:]+"\n")
+                if (found == 0):
+                    sys.stdout.write(x[5:]+": not found\n")
         else:
-            sys.stdout.write(x+": command not found")
+            sys.stdout.write(x+": command not found\n")
 
 
 if __name__ == "__main__":
